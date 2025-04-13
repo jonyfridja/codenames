83bd2376d28604908052f1392f7405464c054a81
Jonathan Fridja <jonyfridja@gmail.com>
2025-04-13 18:56:42 +0300

    vite conf

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3001,
+    host: "127.0.0.1",
+  },
+  base: "/codenames",
 })
