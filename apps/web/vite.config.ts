0eb7c23887f6308173142c5257015fab76f26b7b
Jonathan Fridja <jonathan@qbiq.com>
2026-04-03 18:26:00 +0700

    chore: removed base

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
@@ -12,5 +12,4 @@ export default defineConfig({
   build: {
     outDir: "../../dist",
   },
-  base: "/codenames",
 });
