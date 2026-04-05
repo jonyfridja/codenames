be8d5de6bd4fceea280616408e7593dcf5339711
Jonathan Fridja <jonathan@qbiq.com>
2026-04-05 23:17:06 +0700

    chore: deploy

--- apps/backend/src/server.js
+++ apps/backend/src/server.js
@@ -2,6 +2,8 @@ import { createApp } from "./app.js";
 
 const app = createApp();
 const port = process.env.PORT || 3000;
+console.debug(`PORT env var: ${process.env.PORT}`);
+console.debug(`port: ${port}`);
 
 app.listen(port, () => {
   console.log(`Backend listening on http://localhost:${port}`);
