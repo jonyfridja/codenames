ed6ba0216ac363bf8049b8d5007d68b435d74183
Jonathan Fridja <jonathan@qbiq.com>
2026-04-05 23:26:34 +0700

    fix(backend): port default is 10k

--- apps/backend/src/server.js
+++ apps/backend/src/server.js
@@ -1,7 +1,7 @@
 import { createApp } from "./app.js";
 
 const app = createApp();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 console.debug(`PORT env var: ${process.env.PORT}`);
 console.debug(`port: ${port}`);
 
